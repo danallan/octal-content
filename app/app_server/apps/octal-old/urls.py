a8feb4335dc104aa9db5a1a88f0c9256ae9674a6
Dan Armendariz <danallan@cs.berkeley.edu>
2013-12-11 23:39:44 -0800

    'oh, kill it, it's trash'

--- app/app_server/apps/octal-old/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from django.views.generic.base import TemplateView
-
-from views import get_octal_app
-
-urlpatterns = patterns('',
-                       url(r'^(?i)concepts/', get_octal_app, name="concepts"),
-)
